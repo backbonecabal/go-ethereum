dbccb13a2b4fcb71950259ecd092234efb829bc2
Austin Roberts <austin.roberts@openrelay.xyz>
2021-01-15 15:27:08 -0600

    ethdb/overlay: Fix replay of overlay batch into non-overlay
    When an overlay batch is being replayed into a target that is not
    an overlaywrapper, do not pass the Put()s and Delete()s of objects
    prefixed with "deleted/"

--- ethdb/overlay/batch.go
+++ ethdb/overlay/batch.go
@@ -1,6 +1,7 @@
 package overlay
 
 import (
+	"bytes"
 	"github.com/ethereum/go-ethereum/ethdb"
 )
 
@@ -18,6 +19,12 @@ func (b *Batch) Reset() {
 	b.batch.Reset()
 }
 func (b *Batch) Replay(w ethdb.KeyValueWriter) error {
+	switch w.(type) {
+	case *OverlayWrapperDB:
+		b.batch.Replay(w)
+	default:
+		b.batch.Replay(&nonoverlayReplayer{db: w})
+	}
 	return b.batch.Replay(w)
 }
 func (b *Batch) Put(key []byte, value []byte) error {
@@ -33,4 +40,21 @@ func (b *Batch) Delete(key []byte) error {
 		return err
 	}
 	return b.batch.Put(deleted(key), []byte{})
+}
+
+type nonoverlayReplayer struct {
+	db ethdb.KeyValueWriter
+}
+
+func (r *nonoverlayReplayer) Put(key, value []byte) error {
+	if !bytes.HasPrefix(key, []byte("deleted/")) {
+		return r.db.Put(key, value)
+	}
+	return nil
+}
+func (r *nonoverlayReplayer) Delete(key []byte) error {
+	if !bytes.HasPrefix(key, []byte("deleted/")) {
+		return r.db.Delete(key)
+	}
+	return nil
 }
